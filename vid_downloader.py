cf8aa10684e9ecbd308408089d3509b92eb033c3
Angel Oropeza <31115892+AngelOropeza@users.noreply.github.com>
2020-09-11 20:36:01 -0500

    Añadimos vid_downloader
    vid_downloader recibe una url de un video de youtube, descarga su audio, sube los elementos a S3.

---
+++ vid_downloader.py
@@ -0,0 +1,59 @@
+from pytube import YouTube
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL
+    return response
+
+def vid_downloader(url_video):
+    yt = YouTube(url_video)
+    stream = yt.streams.get_audio_only()
+    stream.download('./audio',"audioextract") 
+    upload_file('audio/audioextract.mp4',"businessoft","audiobject.mp4")
+    return create_presigned_url("businessoft","audiobject.mp4")
+
+    
